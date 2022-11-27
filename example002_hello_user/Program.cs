c73979cd4b987a61419f7d4892de544de9b8afbd
Dmitry An <dmitry.andreyev@gmail.com>
2022-11-27 14:09:41 +0300

    example002 commit

---
+++ example002_hello_user/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Please, enter your name: ");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.WriteLine(username);
