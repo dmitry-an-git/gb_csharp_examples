81f7b5240edd1bde08d3e1a11316a57d8f8b5726
Dmitry An <dmitry.andreyev@gmail.com>
2022-12-01 11:06:09 +0300

    example 009 max in array with max of 3 arg

---
+++ example009_intro_array/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+int[] my_array = {1,2,3,4,5,6,7,8,9};
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int max = Max(Max(my_array[0],my_array[1],my_array[2]),
+    Max(my_array[3],my_array[4],my_array[5]),
+    Max(my_array[6],my_array[7],my_array[8]));
+
+Console.WriteLine(max);
