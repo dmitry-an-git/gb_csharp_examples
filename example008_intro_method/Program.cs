e5f9a82e30c928264d71349deb40f833d45bd4ee
Dmitry An <dmitry.andreyev@gmail.com>
2022-12-01 10:44:16 +0300

    example 008 max through if

---
+++ example008_intro_method/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+int a1 = 1;
+int a2 = 13;
+int a3 = 13;
+int a4 = 15;
+int a5 = 12;
+int a6 = 12;
+int a7 = 14;
+int a8 = 41;
+int a9 = 31;
+
+int max = a1;
+
+if (a2>max) max = a2;
+if (a3>max) max = a3;
+
+if (a4>max) max = a4;
+if (a5>max) max = a5;
+if (a6>max) max = a6;
+
+if (a7>max) max = a7;
+if (a8>max) max = a8;
+if (a9>max) max = a9;
+
+Console.WriteLine(max);
