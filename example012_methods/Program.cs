d6224d0c1f24398b041603ba03e04742883afb2f
Dmitry An <dmitry.andreyev@gmail.com>
2022-12-21 12:19:59 +0300

    example013 commit

---
+++ example012_methods/Program.cs
@@ -0,0 +1,72 @@
+﻿// string method4(int Count, string text)
+// 	{
+// 		int i = 0;
+// 		string result = String.Empty;
+// 		while (i<Count)
+// 			{result += text;
+// 			i++;}
+// 		return result;
+// 	}
+
+// string res = method4(10, "qwerty");
+// Console.WriteLine(res);
+
+// sorting
+int n=10;
+int[] my_array = new int[n];
+Random r = new Random();
+for (int i=0;i<10;i++)
+    {
+        my_array[i]=r.Next(0,1000);
+    }
+
+void PrintArray(int[] array)
+{
+    for (int i=0;i<array.Length; i++)
+        {Console.Write($"{array[i]} ");}
+    Console.WriteLine();
+}
+
+void SelectionSortMin(int[] array)
+{
+    for (int i=0;i<array.Length;i++)
+    {
+        int min = array[i];
+        int min_ind = i;
+        for (int j=i+1;j<array.Length;j++)
+            if(min>array[j])
+                {
+                    min = array[j];
+                    min_ind = j;
+                }
+        array[min_ind] = array[i];
+        array[i] = min;
+    }
+}
+
+void SelectionSortMax(int[] array)
+{
+    for (int i=0;i<array.Length;i++)
+    {
+        int max = array[i];
+        int max_ind = i;
+        for (int j=i+1;j<array.Length;j++)
+            if(max<array[j])
+                {
+                    max = array[j];
+                    max_ind = j;
+                }
+        array[max_ind] = array[i];
+        array[i] = max;
+    }
+}
+
+PrintArray(my_array);
+
+SelectionSortMin(my_array);
+
+PrintArray(my_array);
+
+SelectionSortMax(my_array);
+
+PrintArray(my_array);
---
+++ example013_recursion/Program.cs
@@ -0,0 +1,128 @@
+﻿// 2d arrays
+
+//// text arrays
+// string[,] table = new string[2,5];
+
+// table[1,2] = "word";
+
+// for (int row = 0; row < 2; row++)
+// {
+//     for (int column = 0; column < 5; column++)
+//     {
+//         Console.WriteLine($"-{table[row,column]}-");
+//     }
+// }
+
+// // number arrays
+// int[,] my_matrix = new int[3,4];
+
+// void PrintArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FillArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i,j] = new Random().Next(0,11);
+//         }
+//     }
+// }
+
+// PrintArray(my_matrix);
+// FillArray(my_matrix);
+// Console.WriteLine();
+// PrintArray(my_matrix);
+
+// arrays to pic
+
+// int[,] pic = new int[,]
+// {
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+
+// void ImagePrint(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if(matrix[i,j]==1) Console.Write(" X");
+//             else if(matrix[i,j]==2) Console.Write("//");
+//             else Console.Write("  ");
+//         }
+//         Console.WriteLine();
+//     }
+    
+// }
+
+// void FillImage(int row, int col)
+// {
+//     if (pic[row,col] == 0)
+//     {
+//         pic[row,col] = 2;
+//         FillImage(row-1, col);
+//         FillImage(row, col-1);
+//         FillImage(row+1, col);
+//         FillImage(row, col+1);
+//     }
+// }
+
+// FillImage(13,13);
+// ImagePrint(pic);
+
+// // task Factorial
+
+// double Factorial(int n)
+// {
+//     if (n == 0) return 1;
+//     else return n*Factorial(n-1);
+// }
+// for (int i = 0; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// // task Fibonaci
+// int Fibonaci(int n)
+// {
+//     if (n==1 || n==2) return 1;
+//     else return Fibonaci(n-1)+Fibonaci(n-2);
+// }
+
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine(Fibonaci(i));
+// }
